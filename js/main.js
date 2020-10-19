2239f6797bdaac4bd236595560b84c5b413f1c3c
Estibaliz Barato <estibalizbarato@gmail.com>
2020-10-19 16:45:41 +0200

    fix run funtion game

--- js/main.js
+++ js/main.js
@@ -3,34 +3,31 @@ const tracks = document.querySelector(".js-trackUser");
 const attempts = document.querySelector(".js-attemptsUser");
 const btnPoof = document.querySelector(".js-btnProof");
 const introNumber = document.querySelector(".js-numberUser");
-const numberRamdon = getRandomNumber(100);
-let counterclick = 0;
 
-// action
+let numberRamdon = getRandomNumber(100);
+let counterProof = 0;
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 console.log(numberRamdon);
 
-// paint
-
 function counterBtn() {
-  counterclick++;
+  counterProof++;
   attempts.innerHTML = "Número de intentos: ";
-  attempts.innerHTML += counterclick;
+  attempts.innerHTML += counterProof;
 }
-btnPoof.addEventListener("click", counterBtn);
-
 function checkNumberRandom() {
   let numberValue = parseInt(introNumber.value);
-  if ((numberValue = numberRamdon)) {
+  if (numberValue < 0 && numberValue > 100) {
+    tracks.innerHTML = "El número debe estar entre 1 y 100";
+  } else if (numberValue === numberRamdon) {
     tracks.innerHTML = "Has ganado campeona!!!";
   } else if (numberValue < numberRamdon) {
     tracks.innerHTML = "Demasiado bajo";
   } else if (numberValue > numberRamdon) {
     tracks.innerHTML = "Demasiado alto";
-  } else if (numberValue < 0 || numberValue >= 100) {
-    tracks.innerHTML = "El número debe estar entre 1 y 100";
   }
+  counterBtn();
 }
-checkNumberRandom();
+btnPoof.addEventListener("click", checkNumberRandom);
